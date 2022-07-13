4df0d15f2e03a587e308fab94d9dd401abf42f21
Yuriy <yurchest@email.com>
2022-07-14 01:19:24 +0300

    aboba

--- yurchest/main/parse_news_yandex.py
+++ yurchest/main/parse_news_yandex.py
@@ -3,7 +3,41 @@ from bs4 import BeautifulSoup as bs
 from .models import News
 import time
 import threading
+from datetime import date, timedelta
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 
+def get_source_html(url):
+    driver = webdriver.Chrome(
+        executable_path="/home/yuriy/ProgrammingProjects/1stDjangoSite/yurchest/main/chromedriver_linux64/chromedriver"
+    )
+    driver.maximize_window()
+
+    try:
+        driver.get(url=url)
+        time.sleep(3)
+        clicked = False
+        actions = ActionChains(driver)
+        while True:
+            down_to = driver.find_element(By.CLASS_NAME, "footer__copyright-col")
+            actions.move_to_element(down_to).perform()
+            if not clicked:
+                button_more = driver.find_element(By.CLASS_NAME, "list-more")
+                time.sleep(3)
+                actions.click(button_more).perform()
+                clicked = True
+
+    except Exception as _ex:
+        print(_ex)
+    finally:
+        driver.close()
+        driver.quit()
+
+
+
+def main():
+    get_source_html(url="https://ria.ru/20220101/")
 
 def parse(url):
     r = requests.get(url)
@@ -19,8 +53,10 @@ def parse(url):
         # result_list['href'].append(news.get('href'))
         # result_list['content'].append(news.text)
 
+
 def thread_func():
-        parse(url='https://ria.ru/lenta/')
+        parse(url='https://ria.ru/20220101/')
         threading.Timer(30, thread_func).start()
 
 thread_func()
+# main()
